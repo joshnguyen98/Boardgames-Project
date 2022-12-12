c9b1e823a5b98fe278b0054852351731a6df53db
joshnguyen98 <josh.nguyen@kpmg.co.uk>
2022-12-12 15:02:12 +0000

    finalised 3-getcategories

--- server/app.js
+++ server/app.js
@@ -1,9 +1,7 @@
 const express = require("express");
-const {} = require("./controllers/controllers.errors");
 const { getCategories } = require("./controllers/controllers.games");
 
 const app = express();
-app.use(express.json());
 
 app.get("/api/categories", getCategories)
 
--- server/models/models.games.js
+++ server/models/models.games.js
@@ -1,10 +1,8 @@
 const db = require("../../db/connection");
 
 const selectCategories = () => {
-    console.log("here")
     return db.query(`SELECT * FROM categories`)
     .then((result) => {
-        console.log(result.rows)
         return result.rows
     })
 };
