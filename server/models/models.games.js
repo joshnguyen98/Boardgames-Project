07591bb38328b7e1c778b284e3a7e4da5e62d87d
joshnguyen98 <josh.nguyen@kpmg.co.uk>
2023-01-11 16:53:34 +0000

    added some hardcoded categories

--- server/models/models.games.js
+++ server/models/models.games.js
@@ -13,7 +13,7 @@ exports.selectCategories = () => {
 exports.selectReviews = (category, sort_by='created_at', order='desc') => {
     const validOrderQueries = ['asc', 'desc']
     const validSortByQueries = ['title', 'designer', 'owner', 'review_img_url', 'category', 'created_at', 'votes', 'review_id']
-    const validCategoryQueries = ['euro game', 'social deduction', 'dexterity', "children's games"]
+    const validCategoryQueries = ['euro game', 'social deduction', 'dexterity', "children's games", "strategy", "hidden-roles", "push-your-luck", "roll-and-write", "deck-building", "engine-building"]
 
     if(!validSortByQueries.includes(sort_by) || !validOrderQueries.includes(order) ) {
         return Promise.reject({status: 400, msg: 'Bad Request.'});
